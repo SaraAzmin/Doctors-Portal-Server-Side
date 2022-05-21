ad3d28b87a1a079d98b559bcb7ea8b84c7df31a0
SaraAzmin <azmin.sara17@gmail.com>
2022-05-21 21:52:53 +0200

    data loaded from database

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 //midddlewere
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ecln5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        //all data loaded
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from doctor!')
