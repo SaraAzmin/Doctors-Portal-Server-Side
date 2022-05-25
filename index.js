d2dab8cfa9bbf93e0f546f3f3541cb30b6183e66
SaraAzmin <azmin.sara17@gmail.com>
2022-05-25 23:48:22 +0200

    payment intent added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //midddlewere
 app.use(cors());
@@ -57,6 +58,19 @@ async function run() {
             }
 
         }
+
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret });
+        });
 
         //all services loaded
         app.get('/service', async (req, res) => {
