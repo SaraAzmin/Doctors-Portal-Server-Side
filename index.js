7868af2ba709a8c2fde7e7358d0294fd1b69932a
SaraAzmin <azmin.sara17@gmail.com>
2022-05-26 13:03:35 +0200

    minor changes

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //midddlewere
-app.use(cors());
+//app.use(cors());
+const corsConfig = {
+    origin: true,
+    credentials: true,
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ecln5.mongodb.net/?retryWrites=true&w=majority`;
